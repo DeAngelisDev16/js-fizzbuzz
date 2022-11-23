5239015e98bde2e51ff12aa7e922ce59c6d84644
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-23 14:50:20 +0100

    Stampo solo fizz e buzz (verifica funzionamento)

--- myscript.js
+++ myscript.js
@@ -17,12 +17,24 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 /* stampo i numeri da uno a 100 */
 for(let index = 1; index <= 100; index++ ){
-    console.log(index);
-
-    
+    if (index % 3 === 0){
+        console.log('Fizz');
+    } else if (index % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(index);
+    }
     
 }
+    
+
+    
 
 /* se index % 3 da resto 0 allora stampa fizz
 se index % 5 da resto 0 allora stampa buzz
 se index % 3 && % 5 da resto 0 allora stampa fizzbuzz */
+
+
+/* per verificare che un numero sia divisibile per un altro prendo il primo numero % secondo numero === 0
+    ex 7 % 2 ==>1 allora 7 non è divisibile per 2.
+    quindi a % b === 0; */
