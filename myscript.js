477c6fba38a8e6c95f2d223dc2fb672d33f0e6db
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-23 14:30:21 +0100

    stampo numeri da 1 a 100 + impostazione ragionamento

---
+++ myscript.js
@@ -0,0 +1,28 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100:
+
+
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e
+
+
+per i multipli di 5 stampi “Buzz”.
+
+
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+
+
+
+
+
+/* stampo i numeri da uno a 100 */
+for(let index = 1; index <= 100; index++ ){
+    console.log(index);
+
+    
+    
+}
+
+/* se index % 3 da resto 0 allora stampa fizz
+se index % 5 da resto 0 allora stampa buzz
+se index % 3 && % 5 da resto 0 allora stampa fizzbuzz */
