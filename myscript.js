b7a8341271a233bf1589f650bbb0400240491c81
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-23 15:54:35 +0100

    Inserimento condizione particolare riga 20

--- myscript.js
+++ myscript.js
@@ -16,25 +16,26 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
 /* stampo i numeri da uno a 100 */
-for(let index = 1; index <= 100; index++ ){
-    if (index % 3 === 0){
+for(let index = 1; index <= 100; index++ ){   
+    if (index % 3 === 0 && index % 5 === 0 ){   //condizione particolare va letta prima!!!
+        console.log('FizzBuzz'); 
+
+    } else if (index % 3 === 0){
         console.log('Fizz');
+
     } else if (index % 5 === 0) {
         console.log('Buzz');
-       
+      
     } else  {
         console.log(index);
     }
 
-    if (index % 3 === 0 && index % 5 === 0 ){
-        console.log('FizzBuzz');
-        
-    }
+}
+    
      
         
     
     
-}
     
 
     
