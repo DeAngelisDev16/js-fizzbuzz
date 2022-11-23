edc9800fe3aad5037e86e82553fe6cc627515431
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-23 17:02:49 +0100

    Bonus 1

--- myscript.js
+++ myscript.js
@@ -1,16 +1,7 @@
-/* Scrivi un programma che stampi in console i numeri da 1 a 100:
 
 
-ma che per i multipli di 3 stampi “Fizz” al posto del numero e
-
-
-per i multipli di 5 stampi “Buzz”.
-
-
-Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-
-
+const myDiv = document.getElementById('myDiv');
+console.log(myDiv);
 
 
 
@@ -18,19 +9,34 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 /* stampo i numeri da uno a 100 */
 for(let index = 1; index <= 100; index++ ){   
     if (index % 3 === 0 && index % 5 === 0 ){   //condizione particolare va letta prima!!!
-        console.log('FizzBuzz'); 
+        console.log('FizzBuzz');
+        myDiv.append('FizzBuzz');
+         
 
     } else if (index % 3 === 0){
         console.log('Fizz');
+        myDiv.append('Fizz');
+        
 
     } else if (index % 5 === 0) {
         console.log('Buzz');
+        myDiv.append('Buzz');
+        
       
     } else  {
         console.log(index);
+        myDiv.append(index);
     }
 
 }
+    
+   
+
+
+
+
+
+
     
      
         
